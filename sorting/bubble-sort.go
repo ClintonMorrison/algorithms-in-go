4db6bbda3222faf799f3774f3c7ea37cf6640720
Clinton Morrison <contact@clintonmorrison.com>
2018-02-07 01:32:21 -0500

    run bubble-sort through go fmt tool

--- sorting/bubble-sort.go
+++ sorting/bubble-sort.go
@@ -3,24 +3,23 @@ package main
 import "fmt"
 
 func main() {
-  nums := []int{1, 6, 4, 2, 9, 5, 8, 4, 1, 0}
-  bubblesort(nums)
-  fmt.Println(nums)
+	nums := []int{1, 6, 4, 2, 9, 5, 8, 4, 1, 0}
+	bubblesort(nums)
+	fmt.Println(nums)
 }
 
 func bubblesort(array []int) {
-  for {
-    swapped := false
-    for i := 0; i < len(array) - 1; i += 1 {
-      if (array[i] > array[i+1]) {
-        array[i], array[i+1] = array[i+1], array[i]
-        swapped = true
-      }
-    }
+	for {
+		swapped := false
+		for i := 0; i < len(array)-1; i += 1 {
+			if array[i] > array[i+1] {
+				array[i], array[i+1] = array[i+1], array[i]
+				swapped = true
+			}
+		}
 
-    if !swapped {
-      return;
-    }
-  }
+		if !swapped {
+			return
+		}
+	}
 }
-
