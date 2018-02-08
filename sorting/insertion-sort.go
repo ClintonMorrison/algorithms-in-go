7b3b1d6d090aa15bd78d2bc2a07ec78729459ba0
Clinton Morrison <contact@clintonmorrison.com>
2018-02-07 23:10:39 -0500

    refactor insertion sort

--- sorting/insertion-sort.go
+++ sorting/insertion-sort.go
@@ -6,18 +6,22 @@ func main() {
 	nums := []int{1, 6, 4, 2, 9, 5, 8, 4, 1, 0}
 	insertionSort(nums)
 	fmt.Println(nums)
-
 }
 
 func insertionSort(nums []int) {
 	for i := 1; i < len(nums); i += 1 {
-		numToInsert := nums[i]
-
-		j := i - 1
-		for ; j >= 0 && nums[j] >= numToInsert; j -= 1 {
-			nums[j + 1] = nums[j]
-		}
-
-		nums[j+1] = numToInsert
+		insertIntoSorted(nums, i-1, nums[i])
 	}
+}
+
+func insertIntoSorted(nums []int, sortedUpToIndex int, numToInsert int) {
+	i := sortedUpToIndex
+
+	// Shift numbers in sorted portion right until a number less than numToInsert is found
+	for i >= 0 && nums[i] > numToInsert {
+		nums[i+1] = nums[i]
+		i -= 1
+	}
+
+	nums[i+1] = numToInsert
 }
